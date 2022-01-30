6437cfbe1856a0fce906809256a784e45c26f050
JK-Siddhartha <sidvir123@gmail.com>
2022-01-30 18:34:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import Contact from "./Components/Navbar/Contact";
 import Login from "./Components/Navbar/Login";
 import Register from "./Components/Navbar/Register";
 import Service from "./Components/Navbar/Service";
-import { Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" component={Homepage} exact />
-        <Route path="/about" component={About} exact />
-        <Route path="/service" component={Service} exact />
-        <Route path="/contact" component={Contact} exact />
-        <Route path="/login" component={Login} exact />
-        <Route path="/register" component={Register} exact />
-      </Switch>
+      <Router>
+        <Switch>
+          <Route path="/" component={Homepage} exact />
+          <Route path="/about" component={About} exact />
+          <Route path="/service" component={Service} exact />
+          <Route path="/contact" component={Contact} exact />
+          <Route path="/login" component={Login} exact />
+          <Route path="/register" component={Register} exact />
+        </Switch>
+      </Router>
     </div>
   );
 }
